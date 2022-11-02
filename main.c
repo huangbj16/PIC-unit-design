9c8a79b42d66acf07f10c672781911182ac36e55
Bingjian Huang <huangbj16@gmail.com>
2022-11-02 11:47:03 -0400

    add serial command processing

--- main.c
+++ main.c
@@ -10,30 +10,19 @@
 #include "config.h"
 #include <pic16f15214.h>
 
-#define _XTAL_FREQ 1000000
+#define _XTAL_FREQ 4000000
 
 uint8_t temp = 0;
 unsigned char data = 0;
 uint8_t counter = 0;
+uint8_t enable = 0;
 
 void main(void) {
+    //set internal oscillator to 4MHz
     OSCFRQ = 0x02;
     HFOEN = 1;
     TRISA &= (0b11111011);
     RA2 = 0;
-//    RA5 = 0;
-//    RA4 = 0;
-//    while(1){
-//        for(int i=0; i<160; i++){
-//            RA5 = 1; RA4 = 0;
-//            __delay_ms(3);
-//            RA5 = 0; RA4 = 1;
-//            __delay_ms(3);
-//        }
-//        RA5 = 1; RA4 = 1;
-//        RA5 = 0; RA4 = 0;
-//        __delay_ms(1000);
-//    }
     
     //setup timer0 = 1MHz for interrupt
     T0CON0 = (0b10000000);
@@ -78,14 +67,28 @@ void __interrupt() ISR(void) {
     if(RC1IF == 1){
         data = RC1REG;
         RC1IF = 0;
-        TX1IE = 1;//enable writing
+        if(data == 0xFF){
+            if(enable == 1){
+                //start trigger motor
+                enable = 0;//reset state
+            }
+            TX1IE = 1;
+        }
+        else{
+            data--;
+            if(data == 0){
+                enable = 1;//command to enable current board
+            }
+            else{
+                TX1IE = 1;//enable writing, send to next
+            }
+        }
     }
-    if(TX1IF == 1){// write new data to TX1REG
+    if(TX1IF == 1 && TX1IE == 1){// write new data to TX1REG
         if(data != 0){
             TX1REG = data;
             data = 0;
             TX1IE = 0;//disable writing
-//            TX1IF = 0;
         }
     }
 //    if(TMR0IF == 1){
